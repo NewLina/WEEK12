3f9d172fdb9e248ed2927bb4efc3955693220b9d
Имя <prybak497@gmail.com>
2023-01-18 22:00:41 +0300

    fixed

--- assignment1/assets/index.js
+++ assignment1/assets/index.js
@@ -5,19 +5,25 @@ let avatar=document.querySelector('.avatar');
 let username=document.querySelector('.comment__username');
 let comment=document.querySelector('.comment__content');
 let button=document.querySelector('.button');
-let chat=document.querySelector('.chat__comment');
+let chat=document.querySelector('.chat-block');
 
 
 button.addEventListener('click', publishComment); 
 function publishComment() {
     avatar.src=pictureInput.value;
+
     let usernameValue=usernameInput.value;
-    let firstChar = usernameValue.charAt(0);
-const capitalized = usernameValue.replace(firstChar, firstChar.toUpperCase());
-console.log(capitalized);
-    username.textContent=capitalized;
-    comment.textContent=commentInput.value;
+    let words=usernameValue.split(" ");
+    console.log(words);
+//    let n =words.toLowerCase();
+//    console.log(n);
+let commentValue=commentInput.value;
+let filter = commentValue.replace(/viagra|XXX/ig, "***");
+comment.textContent=filter;
+
+chat.style.display='block';
 }
+
 button.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    });
+evt.preventDefault();
+});
