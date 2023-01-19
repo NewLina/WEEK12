1294e37b11ee2f0063df0e5c635d348d7a0ab20a
Имя <prybak497@gmail.com>
2023-01-19 13:24:00 +0300

    done

--- assignment1/assets/index.js
+++ assignment1/assets/index.js
@@ -6,24 +6,29 @@ let username=document.querySelector('.comment__username');
 let comment=document.querySelector('.comment__content');
 let button=document.querySelector('.button');
 let chat=document.querySelector('.chat-block');
+let form=document.querySelector('.form__body');
 
 
 button.addEventListener('click', publishComment); 
 function publishComment() {
     avatar.src=pictureInput.value;
-
-    let usernameValue=usernameInput.value;
-    let words=usernameValue.split(" ");
-    console.log(words);
-//    let n =words.toLowerCase();
-//    console.log(n);
+let usernameValue=usernameInput.value;
+let modifier=usernameValue.toLowerCase();
+let cleanModifier=modifier.trim();
+function capitalize(cleanModifier) {
+    return cleanModifier.replace(/(^|\s)\S/g, function(upperCase) {return upperCase.toUpperCase()})
+}
+username.textContent=capitalize(cleanModifier);
 let commentValue=commentInput.value;
 let filter = commentValue.replace(/viagra|XXX/ig, "***");
 comment.textContent=filter;
-
 chat.style.display='block';
 }
 
 button.addEventListener('click', function (evt) {
-evt.preventDefault();
-});
+    evt.preventDefault();
+    usernameInput.value=""; 
+    pictureInput.value="";
+    commentInput.value="";
+    });
+
